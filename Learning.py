4b4d5d93a53cbeadaf2c5a93e9b75d50e696a35a
Jorge Roa <45723430+roaj@users.noreply.github.com>
2019-09-13 00:57:48 -0500

    Add files via upload

---
+++ Learning.py
@@ -0,0 +1,18 @@
+# import sys
+
+# print(sys.version)
+
+# x = 43
+# # print('Hello {}'.format(x))
+# # print('Hello %d' %x) 
+
+x= 43
+y= 73
+
+if x < y :
+    print('x < y: x is {} and y is {}'.format(x,y))
+    y = 77
+    print('y = {}'.format(y))
+print('y = {}'.format(y))    
+print('something')
+
