a18b74fe84f2a5add1f3eed43a1caedfac4b90f1
Jorge Roa <45723430+roaj@users.noreply.github.com>
2020-03-07 00:27:51 -0600

    Update 02_03.py

--- Exercise Files/Ch02/02_03 Begin/02_03.py
+++ Exercise Files/Ch02/02_03 Begin/02_03.py
@@ -5,5 +5,11 @@ black = np.zeros([150,200,1],'uint8')#creates an array of zeros / firt imput is 
 cv2.imshow("Black",black)
 print(black[0,0,:])#we loop at the first pixel 0,0 : to see all the values at that location
 
+ones = np.ones([150,200,3],'uint8') #
+cv2.imshow("ones",ones)
+print(ones[0,0,:])
+
+
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
