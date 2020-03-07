63b90721dcc08b4c99929cc9f828d674451552c3
Jorge Roa <45723430+roaj@users.noreply.github.com>
2020-03-07 00:23:59 -0600

    first

---
+++ Exercise Files/Ch02/02_03 Begin/02_03.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np  
+
+black = np.zeros([150,200,1],'uint8')#creates an array of zeros / firt imput is the size or shape [raw,colum,channel] / second input is the type of image 'uint8' /
+cv2.imshow("Black",black)
+print(black[0,0,:])#we loop at the first pixel 0,0 : to see all the values at that location
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
