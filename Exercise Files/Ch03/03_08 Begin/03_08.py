549eb7d94f70a40dbe01877da37e66f3b45c16e2
Jorge Roa <45723430+roaj@users.noreply.github.com>
2020-03-11 11:42:32 -0500

    Update 03_08.py

--- Exercise Files/Ch03/03_08 Begin/03_08.py
+++ Exercise Files/Ch03/03_08 Begin/03_08.py
@@ -8,7 +8,7 @@ import cv2
 
 img = cv2.imread("tomatoes.jpg",1)
 
-hsv = cv2.cvtColor(img, cv2.COLOR_BRG2HSV)
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 res, thresh = cv2. threshold(hsv[:,:,0],25,255,cv2.THRESH_BINARY_INV)
 
 cv2.imshow("thresh", thresh)
