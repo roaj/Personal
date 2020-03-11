703e7b1212eba577e83ee819c6fe2a2ee6fa256d
Jorge Roa <roaj@tamu.edu>
2020-03-11 12:11:07 -0500

    Update 03_08.py

--- Exercise Files/Ch03/03_08 Begin/03_08.py
+++ Exercise Files/Ch03/03_08 Begin/03_08.py
@@ -14,8 +14,8 @@ hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 res, thresh = cv2. threshold(hsv[:,:,0],25,255,cv2.THRESH_BINARY_INV)
 cv2.imshow("thresh", thresh)
 #threshold did not work propely, it got part of the background and may look like one thing
-
 edges = cv2.Canny(img,100,70)
 cv2.imshow("Canny",edges)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
