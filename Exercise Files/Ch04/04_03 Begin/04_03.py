d0cf97478dda97c291d63ffbe91771681e98f5d5
Jorge Roa <45723430+roaj@users.noreply.github.com>
2020-03-11 18:47:48 -0500

    Update 04_03.py

--- Exercise Files/Ch04/04_03 Begin/04_03.py
+++ Exercise Files/Ch04/04_03 Begin/04_03.py
@@ -3,3 +3,13 @@ import cv2
 
 template = cv2.imread('template.jpg',0)
 frame = cv2.imread("players.jpg",0)
+
+cv2.imshow("Frame", frame)
+cv2.imshow("Template",template)
+
+result = cv2.matchTemplate(frame,template, cv2.TM_CCOEFF_NORMED)
+
+cv2.imshow("Matching",result)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
