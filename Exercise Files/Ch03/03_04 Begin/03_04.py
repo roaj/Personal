abe4bc1d30f247d085a516765f4f545b971a7c46
Jorge Roa <45723430+roaj@users.noreply.github.com>
2020-03-11 09:23:47 -0500

    1

--- Exercise Files/Ch03/03_04 Begin/03_04.py
+++ Exercise Files/Ch03/03_04 Begin/03_04.py
@@ -4,12 +4,32 @@ import cv2
 img = cv2.imread('faces.jpg',1)
 
 hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+#keeping all the oroginal pixels but just one channel h , s , v
 h = hsv[:,:,0]
 s = hsv[:,:,1]
 v = hsv[:,:,2]
-
+#put all those hsv channels next to each other on the same tab
+#axis=1 to be horizontal
 hsv_split = np.concatenate((h,s,v),axis=1)
 cv2.imshow("hsv split", hsv_split)
+#to convine to filters irs very usefull sometime
+#specially for further image processing tasks
+
+#apply threshold filter to the saturation channels
+#Everything on this channel that has a value of LESS THAN 40 WILL APPEAR AS WHITE
+ret, min_sat = cv2.threshold(s,40,244,cv2.THRESH_BINARY)
+cv2.imshow("Sat_Filter",min_sat
+#Do the same for the HUE CHANNEL
+#inverse of the normal order of the threshold
+#WILL MAKE VALUES FROM 0 TO 15 WHITE
+ret, max_hue = cv2.threshold(h,15,255,cv2.THRESH_BINARY_INV) 
+cv2.imshow("Hue Filter",max_hue)
+
+#combine filters together
+final = cv2.bitwise_and(min_sat,max_hue)
+cv2.imshow("Final",final)
+
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
