23374b442e194100f322ca993b63432f6f4cfc68
Jorge Roa <45723430+roaj@users.noreply.github.com>
2020-03-11 12:09:33 -0500

    Update 03_08.py

--- Exercise Files/Ch03/03_08 Begin/03_08.py
+++ Exercise Files/Ch03/03_08 Begin/03_08.py
@@ -6,12 +6,16 @@ import cv2
 #Object detection algorith look at the rate of change of color on the pixels
 #canny makes a line at high contrast areas in the image 
 
+#we want to isolate each tomato 
 img = cv2.imread("tomatoes.jpg",1)
-
+#we are going to do a threshold to see that it does not work 
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+#theshold using only hue channel
 res, thresh = cv2. threshold(hsv[:,:,0],25,255,cv2.THRESH_BINARY_INV)
-
 cv2.imshow("thresh", thresh)
+#threshold did not work propely, it got part of the background and may look like one thing
 
+edges = cv2.Canny(img,100,70)
+cv2.imshow("Canny",edges)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
