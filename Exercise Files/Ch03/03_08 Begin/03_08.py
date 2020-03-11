c438d53a30c8b92d916637106ff20ab8db24ded1
Jorge Roa <45723430+roaj@users.noreply.github.com>
2020-03-11 11:34:41 -0500

    Update 03_08.py

--- Exercise Files/Ch03/03_08 Begin/03_08.py
+++ Exercise Files/Ch03/03_08 Begin/03_08.py
@@ -1,4 +1,17 @@
 import numpy as np
 import cv2
 
+#EDGE DETECTION
+#CANNY EDGES IS A TYPE OF EDGE DETECTION ALGORITHM to help create  separation of object within the image 
+#Object detection algorith look at the rate of change of color on the pixels
+#canny makes a line at high contrast areas in the image 
+
 img = cv2.imread("tomatoes.jpg",1)
+
+hsv = cv2.cvtColor(img, cv2.COLOR_BRG2HSV)
+res, thresh = cv2. threshold(hsv[:,:,0],25,255,cv2.THRESH_BINARY_INV)
+
+cv2.imshow("thresh", thresh)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
