0e77658a728583f3d1f90e886f586032318778cc
Jorge Roa <roaj@tamu.edu>
2020-03-11 19:38:31 -0500

    Update 04_05.py

--- Exercise Files/Ch04/04_05 Begin/04_05.py
+++ Exercise Files/Ch04/04_05 Begin/04_05.py
@@ -1,4 +1,9 @@
 import numpy as np
 import cv2
 
+#importing image
 img = cv2.imread("faces.jpeg",1)
+#pass it to grayScale
+gray = "haarcascade_frontalface_default.xml"
+
+face_cascade = cv2.CascadeClassifier(path)
