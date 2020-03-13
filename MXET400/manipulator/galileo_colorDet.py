7aef04939b3b533609f65667497860a3a9fb50b6
Jorge Roa <45723430+roaj@users.noreply.github.com>
2020-03-13 00:27:12 -0500

    Update galileo_colorDet.py

--- MXET400/manipulator/galileo_colorDet.py
+++ MXET400/manipulator/galileo_colorDet.py
@@ -10,22 +10,54 @@ camera_input = 0    #define camera imput
 size_w = 240 #resize image width
 size_h = 160 #resize image height 
 
+#    Color Range, described in HSV
+
+v1_min = 30     # Minimum H value
+v2_min = 20     # Minimum S value
+v3_min = 245    # Minimum V value
+
+v1_max = 43     # Maximum H value
+v2_max = 98     # Maximum S value
+v3_max = 255    # Maximum V value
+
+
 #RGB or HSV
 
 filter = 'HSV'
 
 camera = cv2.VideoCapture(camera_input) #Define camera
 camera.set(3, size_w) #Overriding width of frame
-camera.set(3, size_h)  #Overriding height of fram
+camera.set(4, size_h)  #Overriding height of fram
+x = 0  # will describe target location left to right
+y = 0  # will describe target location bottom to top
+radius = 0  # estimates the radius of the detected target
 
 while(True):
 
     ret,image = camera.read()       #Read frame calling it image
-    
+    height, width, channels = image.shape
 
+
+    if not ret:
+        break
+
+    if filter == 'RGB':                     # If image mode is RGB switch to RGB mode
+        frame_to_thresh = image.copy()
+    else:
+        frame_to_thresh = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)    # Otherwise continue reading in HSV
+
+    thresh = cv2.inRange(frame_to_thresh, (v1_min, v2_min, v3_min), (v1_max, v2_max, v3_max))   # Find all pixels in color range
+
+    kernel = np.ones((5,5),np.uint8)                            # Set gaussian blur strength.
+    mask = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)     # Apply gaussian blur
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+
+    # cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2]     # Find closed shapes in image
+    # center = None   # Create variable to store point
 
 
     cv2.imshow("Image",image)
+    cv2.imshow("mask",mask)
     print(image.shape)
 
     ch = cv2.waitKey(1) #run every 1 milisecond / if 10 then would wait 10 miliseconds
