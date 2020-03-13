1bc8794fb86987674537d1545aa29d3eb039f3fd
Jorge Roa <45723430+roaj@users.noreply.github.com>
2020-03-13 00:19:15 -0500

    go

--- MXET400/manipulator/galileo_colorDet.py
+++ MXET400/manipulator/galileo_colorDet.py
@@ -3,7 +3,8 @@ import numpy as np
 import cv2
 
 #Camera
-
+#Original Frame size 
+    #(480,640,3) RGB
 camera_input = 0    #define camera imput
 
 size_w = 240 #resize image width
@@ -14,10 +15,16 @@ size_h = 160 #resize image height
 filter = 'HSV'
 
 camera = cv2.VideoCapture(camera_input) #Define camera
+camera.set(3, size_w) #Overriding width of frame
+camera.set(4,size_h)  #Overriding height of fram
 
 while(True):
 
-    ret,image = camera.read()
+    ret,image = camera.read()       #Read frame calling it image
+    
+
+
+
     cv2.imshow("Image",image)
     print(image.shape)
 
